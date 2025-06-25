a0aa46c0f36c45abbe5f4fcc4b0710c4d834cecf
user.name like : "Soumen Mandal <user.email like :"soumenmandal1080@gmail.com>
2025-06-25 11:11:45 +0530

    first commit

---
+++ basics/if_statement.js
@@ -0,0 +1,11 @@
+let firstName = "soumen";
+let lastName = "mandal";
+
+if (firstName === "soumen" && lastName === "mandal") {
+    console.log("The first name is soumen and the last name is mandal");
+}
+
+let age = 25;
+if (age >= 18) {
+    console.log("You are an adult");
+}
