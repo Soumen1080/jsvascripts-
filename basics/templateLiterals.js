95e2c34dd63c113598c0dd7d8821207ba70ebf49
user.name like : "Soumen Mandal <user.email like :"soumenmandal1080@gmail.com>
2025-07-09 09:53:24 +0530

    Implements template literals for string interpolation
    Replaces string concatenation with template literals for better readability and maintainability.
    
    This change simplifies the output statements by using the modern JavaScript feature of template literals, enhancing code clarity.

--- basics/templateLiterals.js
+++ basics/templateLiterals.js
@@ -2,6 +2,7 @@ let a = 5;
 let b = 10;
 let c = 15;
 console.log("The value of a+b+c is " + (a+b+c));
+// Using template literals
 console.log(`The value of a+b+c is ${a+b+c} `);
 
 output = `The value of a+b+c is ${a+b+c} `;
