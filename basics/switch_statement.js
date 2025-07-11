53673429b34bbb51c672a2a2c8c28cf9c37971f8
user.name like : "Soumen Mandal <user.email like :"soumenmandal1080@gmail.com>
2025-07-11 23:22:50 +0530

    add switch statment

---
+++ basics/switch_statement.js
@@ -0,0 +1,15 @@
+let age = 30;
+switch(age){
+case 18: 
+console.log("you are 18");
+break;
+case 25:
+    console.log("you are 25");
+    break;
+case 30:
+    console.log("you are 30");
+    break;
+default:
+    console.log("you are not 18, 25 or 30");
+    break;
+}
