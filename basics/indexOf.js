e9165e770ed805fd9543b5b3763982f7a6e18502
Soumen Mandala <soumenmandal1080@gmail.com>
2025-08-06 00:03:55 +0530

    good String & indexOf

--- basics/indexOf.js
+++ basics/indexOf.js
@@ -1,5 +1,17 @@
-let str  = " I Love Coding "
-aug = str.indexOf("o");
+// let str  = " I Love Coding ";
+// aug = str.indexOf("o");
 
-console.log("aug");
- 
+
+// console.log(aug);
+//  console.log(str.infexOf("Love"));
+//  console.log(str.infexOf("Z"));
+let str = " I Love Coding ";
+let aug = str.indexOf("o");
+
+
+
+
+//aswe hfu ihaf hsfh isah gfash f
+console.log(aug);
+console.log(str.indexOf("Love"));
+console.log(str.indexOf("Z"));
