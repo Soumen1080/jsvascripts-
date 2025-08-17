c76f4a23a91475708ef1786ca97eaf88ae64100a
Soumen Mandal <soumenmandal1080@gmail.com>
2025-08-17 18:12:59 +0530

    object

---
+++ JS(Part 5)/objectList.js
@@ -0,0 +1,15 @@
+const properties =[ {
+    username : "soumen1080",
+    content : "video",
+    like : "20K",
+    reposts : "2k",
+    tage : ["biluga1080","soumen1080",""],
+},
+ {
+    username : "biluga1080",
+    content : "video",
+    like : "29K",
+    reposts : "7k",
+    tage : ["biluga1080","soumen1080",""],
+},
+]
---
+++ basics/12.fevMovie.js
@@ -0,0 +1,11 @@
+let = movie ;
+movie = prompt("what  is your fev movie(guess) ?");
+fev = "Avenger";
+while (movie != fev && movie != "Quit"){
+
+movie = prompt("what  is your fev movie(guess) ?");
+console.log("Worng guess! Try again");
+
+}
+console.log("Correct guess");
+
