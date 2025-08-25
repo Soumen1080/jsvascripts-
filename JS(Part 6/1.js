a253c54c45e58f05cb194ff9eb5aae5d98d971ab
Soumen Mandal <soumenmandal1080@gmail.com>
2025-08-26 00:26:08 +0530

    array elements

---
+++ JS(Part 6/1.js
@@ -0,0 +1,11 @@
+let arr = [1,2,3,4,5,6,7,8,9,10];
+let n = 5;
+
+function arrElement(arr , n) {
+
+    for(let i = 0 ; i< arr.length ; i++)
+        if(arr[i] > n ){
+            console.log(arr[i]);
+        }
+    
+}
